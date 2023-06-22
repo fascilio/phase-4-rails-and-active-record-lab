9aeb3085a3451bcd3328313fc9728446313e967c
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-06-22 12:51:07 +0300

    Student model passed

---
+++ db/migrate/20230622093901_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :grade, default: 0
+
+      t.timestamps
+    end
+  end
+end
